0dc9594480e218951b48b5358d3ca7795bf3e2df
roccoagain <roccoantoniou@gmail.com>
2024-02-29 22:34:05 -0700

    added .gitignore and updated build/clean scripts

--- clean_all.sh
+++ clean_all.sh
@@ -5,9 +5,7 @@ find ./ -name 'a.out' -type f -delete
 
 find . -type f ! -name "*.*" -perm +111 -exec sh -c '
     for file do
-        # Check if the file is an executable
         if [ -x "$file" ]; then
-            # Delete the file
             echo "Deleting executable: $file"
             rm "$file"
         fi
