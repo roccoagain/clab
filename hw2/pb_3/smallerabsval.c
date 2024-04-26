a5262ab91757e2a6f6a25a0808e2df4fe71145ec
roccoagain <roccoantoniou@gmail.com>
2024-04-26 10:42:47 -0600

    fixed smallerabsval.c

--- hw2/pb_3/smallerabsval.c
+++ hw2/pb_3/smallerabsval.c
@@ -2,33 +2,27 @@
 #include <stdio.h>
 
 int smallerabsval(int x, int y) {
-  if (x < 0) {
-    x *= -1;
-  }
-  if (y < 0) {
-    y *= -1;
-  }
+  int absX = (x < 0) ? -x : x;
+  int absY = (y < 0) ? -y : y;
 
-  if (x <= y) {
+  if (absX < absY) {
     return x;
-  } else {
+  } else if (absY < absX) {
     return y;
+  } else {
+    return (x > 0) ? x : y;
   }
 }
 
 int main() {
-  assert(smallerabsval(3, 5) == 3);
-  printf("smallerabsval(3,5) = %d\n", smallerabsval(3, 5));
-
-  assert(smallerabsval(-2, 5) == 2);
-  printf("smallerabsval(-2,5) = %d\n", smallerabsval(-2, 5));
-
-  assert(smallerabsval(5, 5) == 5);
-  printf("smallerabsval(5,5) = %d\n", smallerabsval(5, 5));
-
-  assert(smallerabsval(-7, -5) == 5);
-  printf("smallerabsval(-7,-5) = %d\n", smallerabsval(-7, -5));
-
-  assert(smallerabsval(-5, -5) == 5);
-  printf("smallerabsval(-5,-5) = %d\n", smallerabsval(-5, -5));
+  assert(smallerabsval(-8, -5) == -5);
+  printf("smallerabsval(-8,-5) = %d\n", smallerabsval(-8, -5));
+  assert(smallerabsval(-5, -8) == -5);
+  printf("smallerabsval(-5,-8) = %d\n", smallerabsval(-5, -8));
+  assert(smallerabsval(5, 8) == 5);
+  printf("smallerabsval(5,8) = %d\n", smallerabsval(5, 8));
+  assert(smallerabsval(8, 5) == 5);
+  printf("smallerabsval(8,5) = %d\n", smallerabsval(8, 5));
+  assert(smallerabsval(-5, 5) == 5);
+  printf("smallerabsval(-5,5) = %d\n", smallerabsval(-5, 5));
 }
